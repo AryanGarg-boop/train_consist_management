1e33e049dfc91e4fd044d565a820b0a2e0b0710f
krish rathi <kr9688@srmist.edu.in>
2026-04-13 10:39:10 +0530

    uc1

--- App/src/trainConsistManagementApp.java
+++ App/src/trainConsistManagementApp.java
@@ -1,2 +1,19 @@
+import java.util.*;
+
 public class trainConsistManagementApp {
+    public static void main(String[] args){
+        System.out.println("=============================================");
+        System.out.println("           Train Consist Management App      ");
+        System.out.println("=============================================");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train initialized successfully....");
+
+        int count = trainConsist.size();
+
+        System.out.println("Initial boogie count : " + count);
+
+        System.out.println("System ready for operations....");
+    }
 }
